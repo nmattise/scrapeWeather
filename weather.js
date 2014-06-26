bf349a5f779fe01bfb41f436de3b75b361496fac
nmattise <n.mattise@gmail.com>
2014-06-25 23:31:41 -0400

    added get full month temperatures

--- weather.js
+++ weather.js
@@ -21,9 +21,28 @@ function getHourlyTemp(date, airport, callback) {
     });
 }
 
-function getMonthlyTemp(startDate, airport, callback) {}
+function getMonthlyTemp(month, airport, callback) {
+    var urlDate = month.split("-"),
+        url = "http://www.wunderground.com/history/airport/" + airport + "/"+urlDate[0]+"/"+urlDate[1]+"/"+urlDate[2]+"/MonthlyHistory.html?format=1",
+        weather = new Object();
+    request(url, function(err, response, body){
+        csv.parse(body, function(err, data){
+            var tempLoc = data[0].indexOf("Mean TemperatureF");
+            data.forEach(function(row){
+                weather[row[0]] = row[tempLoc];
+            });
+            callback(weather);
+        })
+    })
+};
+
+/*getMonthlyTemp('2013-07-11', 'KAVP', function(weather){
+    console.log(weather);
+});*/
 
 function getCustomRangeTemp(startDate, endDate, airport, callback) {}
-getHourlyTemp('2013-07-11', 'KAVP', function (weather) {
+
+
+/*getHourlyTemp('2013-07-11', 'KAVP', function (weather) {
     console.log(weather);
-})
+})*/
