592b3f7120bf5aef9cad5b7595970af8d0411a05
nmattise <n.mattise@gmail.com>
2014-06-25 23:43:23 -0400

    Added Custom Range Mean Temperature

--- weather.js
+++ weather.js
@@ -20,7 +20,9 @@ function getHourlyTemp(date, airport, callback) {
         })
     });
 }
-
+/*getHourlyTemp('2013-07-11', 'KAVP', function (weather) {
+    console.log(weather);
+})*/
 function getMonthlyTemp(month, airport, callback) {
     var urlDate = month.split("-"),
         url = "http://www.wunderground.com/history/airport/" + airport + "/"+urlDate[0]+"/"+urlDate[1]+"/"+urlDate[2]+"/MonthlyHistory.html?format=1",
@@ -40,9 +42,21 @@ function getMonthlyTemp(month, airport, callback) {
     console.log(weather);
 });*/
 
-function getCustomRangeTemp(startDate, endDate, airport, callback) {}
-
-
-/*getHourlyTemp('2013-07-11', 'KAVP', function (weather) {
+function getCustomRangeTemp(startDate, endDate, airport, callback) {
+    var startDate = startDate.split('-'),
+        endDate = endDate.split('-'),
+        url = "http://www.wunderground.com/history/airport/"+airport+"/"+startDate[0]+"/"+startDate[1]+"/"+startDate[2]+"/CustomHistory.html?dayend="+endDate[2]+"&monthend="+endDate[1]+"&yearend="+endDate[0]+"&req_city=NA&req_state=NA&req_statename=NA&format=1",
+        weather = new Object();
+    request(url, function(err, response, body){
+        csv.parse(body, function(err, data){
+            var tempLoc = data[0].indexOf("Mean TemperatureF");
+            data.forEach(function(row){
+                weather[row[0]] = row[tempLoc];
+            });
+            callback(weather);
+        })
+    })
+}
+getCustomRangeTemp('2013-07-11','2013-08-11', 'KAVP', function(weather){
     console.log(weather);
-})*/
+});
