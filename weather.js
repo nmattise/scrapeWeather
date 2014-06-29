c4aee194ddbad2607c9443a96aad9746f3a351b8
nmattise <n.mattise@gmail.com>
2014-06-28 22:59:31 -0400

    first attempt at multi observations weather grab

--- weather.js
+++ weather.js
@@ -42,21 +42,24 @@ function getMonthlyTemp(month, airport, callback) {
     console.log(weather);
 });*/
 
-function getCustomRangeTemp(startDate, endDate, airport, callback) {
+function getCustomRangeTemp(startDate, endDate,requestedResults, airport, callback) {
     var startDate = startDate.split('-'),
         endDate = endDate.split('-'),
         url = "http://www.wunderground.com/history/airport/"+airport+"/"+startDate[0]+"/"+startDate[1]+"/"+startDate[2]+"/CustomHistory.html?dayend="+endDate[2]+"&monthend="+endDate[1]+"&yearend="+endDate[0]+"&req_city=NA&req_state=NA&req_statename=NA&format=1",
         weather = new Object();
+    function weatherDate(row, refRow, reqs){
+        this[row[0]]={};
+        for (var i=0; i<reqs.length;i++){
+            this[row[0]][reqs[i]] = row[refRow.indexOf(reqs[i])];
+        };
+    };
     request(url, function(err, response, body){
         csv.parse(body, function(err, data){
-            var tempLoc = data[0].indexOf("Mean TemperatureF");
-            data.forEach(function(row){
-                weather[row[0]] = row[tempLoc];
-            });
+
             callback(weather);
-        })
-    })
+        });
+    });
 }
-getCustomRangeTemp('2013-07-11','2013-08-11', 'KAVP', function(weather){
+getCustomRangeTemp('2013-07-11','2013-08-11',['Max TemperatureF','Mean TemperatureF','Min TemperatureF'], 'KAVP', function(weather){
     console.log(weather);
 });
